1ba71d5b50cde35f7a57ad59a040c5163aaeab47
Nilu Nassiri <nilu.nassiri@gmail.com>
2023-05-03 21:40:18 +0300

    Add show single row

--- src/layouts/shipment/showShipment.js
+++ src/layouts/shipment/showShipment.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -8,14 +8,36 @@ function ShowShipment() {
   const routeParams = useParams();
   const { orderNo } = routeParams;
   const [shipmentData, setShipmentData] = useState(null);
+  const [date, setDate] = useState(null);
+  const [customer, setCustomer] = useState(null);
+  const [trackingNo, setTrackingNo] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [consignee, setConsignee] = useState(null);
 
-  console.log(shipmentSlicer.status);
   if (shipmentSlicer.status) {
-    setShipmentData(shipmentSlicer.value.filter((shipment) => shipment.orderNo === orderNo));
+    useEffect(() => {
+      setShipmentData(shipmentSlicer.value.filter((shipment) => shipment.orderNo === orderNo));
+    }, []);
   }
+
+  useEffect(() => {
+    if (shipmentData) {
+      setDate(shipmentData[0].date);
+      setCustomer(shipmentData[0].customer);
+      setTrackingNo(shipmentData[0].trackingNo);
+      setStatus(shipmentData[0].status);
+      setConsignee(shipmentData[0].consignee);
+    }
+  }, [shipmentData]);
+
   return (
     <DashboardLayout>
-      <h2>{shipmentData}</h2>
+      <h2>{orderNo}</h2>
+      <h2>{date}</h2>
+      <h2>{customer}</h2>
+      <h2>{trackingNo}</h2>
+      <h2>{status}</h2>
+      <h2>{consignee}</h2>
     </DashboardLayout>
   );
 }
